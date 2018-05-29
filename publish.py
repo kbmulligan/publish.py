6c55552cae2846925f234296efc9abb3dcc0a54f
Brett Mulligan <brett.mulligan@gmail.com>
2018-05-29 07:14:51 -0400

    Enabled extended tweet mode

--- publish.py
+++ publish.py
@@ -8,7 +8,8 @@ import datetime
 import time
 import sys
 import os
-import csv
+import pprint
+# import csv
 
 import twitter
 
@@ -17,24 +18,26 @@ READ_AND_WRITE = 'r+'
 READ_ONLY = 'r'
 WRITE_ONLY = 'w'
 READ_AND_WRITE_NEW_TRUNC = 'w+'
+APPEND = 'a'
 
 CONTINUOUS = False
 VERBOSE = False
 
-seconds = 60            # seconds per interval (default=60)
-interval = 30           # seconds multiplier between tweet checks (default=30)
+COMPATIBILITY = 'compat'
+EXTENDED = 'extended'
+TWEET_MODE = EXTENDED 
 
-REACH_BACK = 15         # how many posts to go back
-latest_tweet_id = 0     # most recent tweet
+REACH_BACK = 15                     # how many posts to go back
 
 KEY_FILENAME = 'keys/keys.txt'
-LATEST_TWEETS_FILENAME = 'latest_unprocessed_tweets.txt'
-LATEST_TWEET_ID_FILENAME = 'latest_tweet_id.txt'
+LATEST_TWEETS_FILENAME = 'latest_tweets.txt'
+LAST_TWEET_ID_FILENAME = 'last_tweet_id.txt'
 LOG_FILENAME = 'publish.log'
 
+
+# database files
 ACTIVITY_FILENAME = '../public_html/currently.txt'
 BOOK_FILENAME = '../public_html/books.txt'
-
 
 # the dict KEYS below are what the program is going to scan twitter for
 # the dict VALUES below are what the program is going write to the text file 
@@ -48,8 +51,10 @@ act_pairs = {        '#reading':         'reading',
 
 HASHTAGS = act_pairs.keys()
 
+# FORMATTING
 ACTIVITY_SEPARATOR = ';'
 BOOK_SEPARATOR = ';'
+STRIP_CHARS = '!?. '
 
 # hashtag which indicates a book has been read
 BOOK_COMPLETE_COMMAND = '#finished'
@@ -101,21 +106,33 @@ class Book:
         return not self.__eq__(other)
 
 
+def extract_full_text(tweet):
+    """ Returns the full text of a tweet. Checks for truncation. """
+
+    if tweet.tweet_mode == EXTENDED:
+        full_text = tweet.full_text
+    else:
+        full_text = tweet.text
+    return convert(full_text)
+
+def convert(unicode_text):
+    return unicode_text.encode(errors='replace')
+
 def get_activity_updates (posts, activities) :
-    """ Scan posts for activity updates and return pairs that qualify in a tuple. 
-    """
+    """ Scan posts for activity updates and return pairs that qualify in a tuple. """
+
     print('Checking for activity updates...')
     print('Scanning {} most recent posts'.format(len(posts)))
     
     updates = []
     for post in posts:
-        unicode_post = post.text.encode(errors='replace')
+
         for hashtag in activities:
-            if hashtag in unicode_post:
-                print(unicode_post)  
+            if hashtag in post:
+                print(post)  
                 
                 # split string at 'act' and take end element
-                new_activity = unicode_post.split(hashtag)[-1].strip().strip('!?;.')
+                new_activity = post.split(hashtag)[-1].strip().strip(STRIP_CHARS)
                 updates.append((hashtag, new_activity))
 
     return tuple(updates)
@@ -140,7 +157,8 @@ def update_activities (activities, filename=ACTIVITY_FILENAME) :
                     # if the keyword is found, replace that line
                     if act_pairs[keyword] == file_content[i].split(ACTIVITY_SEPARATOR)[0]:
                         print('Overwriting a current activity line!')
-                        file_content[i] = '{}{}{}\n'.format(act_pairs[keyword], ACTIVITY_SEPARATOR, subject)
+                        file_content[i] = '{}{}{}\n'.format(
+                                            act_pairs[keyword], ACTIVITY_SEPARATOR, subject)
             
         # write it all back to the file 
         with open(filename, READ_AND_WRITE_NEW_TRUNC) as f:
@@ -158,8 +176,8 @@ def get_book_updates (posts):
 
     updates = []
     for post in posts:
-        if BOOK_COMPLETE_COMMAND in post.text:
-            book = extract_book_from_string(post.text)
+        if BOOK_COMPLETE_COMMAND in post:
+            book = extract_book_from_string(post)
             updates.append(book.as_tuple())
 
     return tuple(updates)
@@ -181,8 +199,8 @@ def update_books (updates, filename=BOOK_FILENAME) :
             # insert new additions at the beginning
             for update in updates:
                 if (book_is_unique(all_books, update)): 
-                    title, author = update
-                    all_books.insert(0, '{1}{0}{2}{0}{3}'.format(BOOK_SEPARATOR, title, 
+                    title, author, _ = update
+                    all_books.insert(0, '{1}{0}{2}{0}{3}\n'.format(BOOK_SEPARATOR, title, 
                                         author, datetime.date.today()))
                     #print('Would write to file here!')
 
@@ -198,20 +216,27 @@ def extract_book_from_string(text):
     # Break tweet at BOOK_COMPLETE_COMMAND and take everything afterward
     book_info = text.encode(errors='replace').split(BOOK_COMPLETE_COMMAND)[-1]
 
-    # Remove trailing whitespace and split at TITLE_AUTHOR_SEPARATOR
-    title_and_author = [x.strip().strip('!.') for x in book_info.strip().split(TITLE_AUTHOR_SEPARATOR)]
+    book_info_and_link = book_info.split("http://")
 
-    title = title_and_author[0]             # first bit 
+    prefixes = ['http://', 'https://']
+    if any(prefix in book_info for prefix in prefixes):
+        for prefix in prefixes:
+            book_info = book_info.split(prefix)[0] 
+
+    # Remove trailing whitespace and split at TITLE_AUTHOR_SEPARATOR
+    title_and_author = [x.strip().strip(STRIP_CHARS) for x in book_info.strip().split(TITLE_AUTHOR_SEPARATOR)]
+
+    title = title_and_author[0]                  # first bit 
     author = "".join(title_and_author[1:])       # the rest of it
 
     return Book(title, author) 
 
 def book_is_unique (all_books, new_book):
-    """ Returns True if new_book is not already in all_books.
-    """
+    """ Returns True if new_book is not already in all_books. """
+    
     new_book_title = new_book[0].lower()
-    recorded_titles = [book.split(book_separator)[0].lower() for book in all_books]
-    return new_book_title in recorded_titles
+    recorded_titles = [book.split(BOOK_SEPARATOR)[0].lower() for book in all_books]
+    return new_book_title not in recorded_titles
 
 def keys_from_file (filename):
     keys = []
@@ -250,8 +275,11 @@ def usage ():
     sys.exit()
     return
 
-def get_latest_tweet_id (tweets) :
+def get_newest_tweet_id (tweets):
     return max([tweet.id for tweet in tweets])
+
+def get_oldest_tweet_id (tweets):
+    return min([tweet.id for tweet in tweets])
 
 def get_keys_and_user ():
     """ This function retrieves access keys and username from system arguments, a specified file,
@@ -300,16 +328,19 @@ def log (info, filename=LOG_FILENAME) :
         log_file.write("{}\n".format(info))
 
 
+def write_tweets_to_file(tweets, filename):
+    """ Write all tweets to filename. """
 
-if __name__ == '__main__':
+    # SET ONLY FOR EXTENDED MODE !!!
+    with open(filename, APPEND) as tweet_file :
+        for (tid, content) in [(post.id, post.full_text.encode(errors='replace')) for post in tweets]:
+            tweet_file.write('{} : {}\n'.format(tid, content))
+    
 
-    user, ck, cs, atk, ats = get_keys_and_user ()
-
-    api = twitter.Api(ck, cs, atk, ats)
-
-    # This section opens LATEST_TWEET_ID_FILENAME and checks the last saved tweet id.
+def read_tweet_id(filename):
+    """ Returns tweet id from filename. """
     try:
-        with open(LATEST_TWEET_ID_FILENAME, READ_ONLY) as tweet_file:
+        with open(filename, READ_ONLY) as tweet_file:
             first_line = tweet_file.readline().strip()
             if first_line:
                 last_tweet_id = int(first_line)
@@ -318,46 +349,60 @@ if __name__ == '__main__':
 
     except IOError:
         try:
-            with open(LATEST_TWEET_ID_FILENAME, READ_AND_WRITE_NEW_TRUNC) as tweet_file :
+            with open(filename, READ_AND_WRITE_NEW_TRUNC) as tweet_file:
                 last_tweet_id = 0
         except:
-            print("Really can't open a file!")
-            
-    # check posts initially
-    latest_posts = api.GetUserTimeline(screen_name=user, since_id=last_tweet_id)
-    if VERBOSE:
-        print("Retrieved {} posts.".format(len(latest_posts)))
+            print("Really can't open that file!")
+            print("Quitting...")
+            exit(1)
 
-    if latest_posts:
-        latest_tweet_id = get_latest_tweet_id(latest_posts)
-        do_update(latest_posts)
+    return last_tweet_id
+    
+def write_tweet_id(tweet_id, filename):
+    """ Write tweet id given to filename. """
+    with open(filename, READ_AND_WRITE_NEW_TRUNC) as f:
+        f.write('{}\n'.format(tweet_id))
+
+
+if __name__ == '__main__':
+
+    # Authenticate
+    user, ck, cs, atk, ats = get_keys_and_user()
+    api = twitter.Api(  consumer_key=ck, consumer_secret=cs, 
+                        access_token_key=atk, access_token_secret=ats, 
+                        tweet_mode=TWEET_MODE)
+
+    last_tweet_id = read_tweet_id(LAST_TWEET_ID_FILENAME)
+
+    # Check posts
+    raw_new_posts = api.GetUserTimeline(screen_name=user, 
+                                    since_id=last_tweet_id,
+                                    include_rts=False,
+                                    trim_user=True
+                                    )
+
+    # Extract text
+    new_posts = [extract_full_text(post) for post in raw_new_posts]
+
+    # Process if any 
+    if new_posts:
+        newest_tweet_id = get_newest_tweet_id(raw_new_posts)
+        do_update(new_posts)
     else:
-        latest_tweet_id = last_tweet_id
+        newest_tweet_id = last_tweet_id
         no_update()
 
-    with open(LATEST_TWEETS_FILENAME, READ_AND_WRITE) as tweet_file :
-        for (content, tid) in [(post.text.encode(errors='replace'), post.id) for post in latest_posts] :
-            tweet_file.write('{} : {}\n'.format(tid, content))
+    # Record what was processed
+    write_tweets_to_file(raw_new_posts, LATEST_TWEETS_FILENAME)
+    write_tweet_id(newest_tweet_id, LAST_TWEET_ID_FILENAME)
+
+    # REPORT ALL
+    if VERBOSE:
+        print("Retrieved {} posts.".format(len(new_posts)))
 
     if VERBOSE:
-        print("Latest tweet id in file: {}".format(last_tweet_id))
-        print("Latest tweet id just retrieved: {}".format(latest_tweet_id))
+        print("Latest tweet id in file:        {}".format(last_tweet_id))
+        print("Latest tweet id just retrieved: {}".format(newest_tweet_id))
 
-    with open(LATEST_TWEET_ID_FILENAME, READ_AND_WRITE) as tweet_file:
-        tweet_file.write('{}\n'.format(latest_tweet_id))
-
-
-    ### CONTINUOUS MODE ######################
-    while CONTINUOUS:
-
-        latest_posts = api.GetUserTimeline(screen_name=user, since_id=latest_tweet_id)
-
-        if latest_posts:
-            latest_tweet_id = get_latest_tweet_id(latest_posts)
-            do_update(latest_posts)
-        else:
-            latest_tweet_id = last_tweet_id
-            no_update()
-
-        # wait
-        time.sleep(seconds*interval)
+    if VERBOSE:
+        print("Complete!")
